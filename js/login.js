70b8c7c1d75920df2911b89f41ad86abe968c440
sofiapascale <sofi.pascale13@gmail.com>
2022-09-02 16:37:30 -0300

    commit validacion

--- js/login.js
+++ js/login.js
@@ -17,3 +17,33 @@ button.addEventListener("click", () => {
   console.log(localStorage.getItem("usuario"))
 })
 
+function showAlertSuccess() {
+  document.getElementById("alert-success").classList.add("show");
+}
+
+function showAlertError() {
+  document.getElementById("alert-danger").classList.add("show");
+}
+
+button.addEventListener("click", function (){
+
+  const regularPhrase = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  let comeIn = false
+
+
+  if(!regularPhrase.test(inputCorreo.value)){
+      comeIn = true
+  };
+
+  if(inputPassword.value.length <= 1){
+      comeIn = true
+  };
+
+  if(comeIn){
+      showAlertError();
+  } else {
+      showAlertSuccess();
+  }
+
+  console.log(comeIn)
+});
